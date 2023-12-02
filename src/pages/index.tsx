2d75942c3bddb510ccbe1b5cbd5079f65fe25820
Jafagervik <jorgenafagervik@gmail.com>
2023-12-02 19:04:54 +0100

    initial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
+import IconButton from '@mui/material/Button';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GithubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Home() {
   return (
@@ -10,7 +14,45 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#FF0000] to-[#00FF00]">
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#DCE35B] to-[#45B649]">
+        
+        <h1 className="font-sans text-5xl">Hi, my name is Jørgen!</h1>
+
+        <p className="text-3xl pt-2">
+            I'm a norwegian software developer, currently writing my master thesis
+            at NTNU.
+
+            Experience: Parallel computing, Software development, AI development, DevOps,
+            Linux, Jira, Confluence, C#, .NET, Julia, Python, C, C++, Rust, Go and more..
+        </p>
+
+        <Link href="https://github.com/Jafagervik">
+            <GithubIcon />
+        </Link>
+
+        <Link href="https://crates.io/crates/ganban"> 
+            <IconButton aria-label="delete" size="large">
+                GanBan 
+            </IconButton>
+        </Link>
+
+        <Link href="https://crates.io/crates/sukker">
+            <IconButton aria-label="delete" size="large">
+                Sukker
+            </IconButton>
+        </Link>
+
+        <Link href="https://www.linkedin.com/in/j%C3%B8rgen-aleksander-fagervik-4556b0173/">
+           <LinkedInIcon /> 
+        </Link>
+
+        <h2 className="text-2xl">Download CV</h2>
+
+        <p>
+            Get in contact!
+            <EmailIcon />
+        </p>
+
 
         </main>
     </>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        serif: ["var(--font-serif)", ...fontFamily.serif],
+        sans: ["var(--font-sans)", ...fontFamily.sans]
       },
     },
   },
